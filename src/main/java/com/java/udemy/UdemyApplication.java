11daf69c2697ab96092fcb316ba1fb83026cf1e6
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-10-05 10:47:48 +0700

    init project

---
+++ src/main/java/com/java/udemy/UdemyApplication.java
@@ -0,0 +1,35 @@
+package com.java.udemy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.jetbrains.annotations.NotNull;
+
+@SpringBootApplication
+public class UdemyApplication {
+	@Value(value = "${frontend.root.url}")
+	private String FRONTEND_URL;
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NotNull CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowCredentials(true)
+						.exposedHeaders("*")
+						.maxAge(3600L)
+						.allowedOriginPatterns("http://localhost:[*]", FRONTEND_URL)
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+			}
+		};
+	}
+
+}
---
+++ src/test/java/com/java/udemy/UdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

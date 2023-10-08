018c1e32451b526936af4a13eccd7e26eaf5e43c
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-10-08 15:20:46 +0700

    update Course Models

--- src/main/java/com/java/udemy/models/Course.java
+++ src/main/java/com/java/udemy/models/Course.java
@@ -1,10 +1,15 @@
 package com.java.udemy.models;
 
 import java.math.BigDecimal;
+import java.util.Objects;
+import java.io.Serializable;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.Hibernate;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.validator.constraints.URL;
 
@@ -12,13 +17,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "courses")
+@Table(name = "courses", indexes = { @Index(name = "IDX_CATEGORY", columnList = "category") })
 @Getter
 @Setter
-public class Course {
+@RequiredArgsConstructor
+public class Course implements Serializable {
+  private static final long serialVersionUID = -2540907171719494221L;
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -57,4 +65,19 @@ public class Course {
   @ColumnDefault(value = "FALSE")
   @Column(nullable = false)
   private Boolean isFeatured = false;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o))
+      return false;
+    Course course = (Course) o;
+    return id != null && Objects.equals(id, course.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
 }

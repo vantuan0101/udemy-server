962e3a131441f14eaf94ff94829b956277141fe9
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-10-08 14:16:40 +0700

    Add CustomResponse models

---
+++ src/main/java/com/java/udemy/models/MyCustomResponse.java
@@ -0,0 +1,32 @@
+package com.java.udemy.models;
+
+/**
+ * Generic response with message
+ */
+public class MyCustomResponse {
+  private final String message;
+  private final Boolean success;
+
+  public MyCustomResponse(String message, Boolean success) {
+    this.message = message;
+    this.success = success;
+  }
+
+  public MyCustomResponse(String message) {
+    this.message = message;
+    this.success = true;
+  }
+
+  public static MyCustomResponse fail(String message) {
+    return new MyCustomResponse(message, false);
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public Boolean getSuccess() {
+    return success;
+  }
+
+}

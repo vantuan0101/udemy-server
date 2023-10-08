98055c943521e3c083b5451b19c3f51a2bef7063
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-10-08 15:22:05 +0700

    update Review Models

--- src/main/java/com/java/udemy/models/Review.java
+++ src/main/java/com/java/udemy/models/Review.java
@@ -1,8 +1,12 @@
 package com.java.udemy.models;
 
 import java.time.Instant;
+import java.util.Objects;
 
+import org.hibernate.Hibernate;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -10,6 +14,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Max;
@@ -18,7 +23,8 @@ import javax.validation.constraints.Size;
 @Entity
 @Getter
 @Setter
-@Table(name = "review")
+@RequiredArgsConstructor
+@Table(name = "reviews", uniqueConstraints = @UniqueConstraint(columnNames = { "user_id", "course_id" }))
 public class Review {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,12 +38,16 @@ public class Review {
   @Size(max = 300)
   private String content;
 
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
   @JsonBackReference
   @JoinColumn(name = "user_id")
+  @OnDelete(action = OnDeleteAction.CASCADE)
   private User user;
 
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
   @JsonBackReference
   @JoinColumn(name = "course_id")
+  @OnDelete(action = OnDeleteAction.CASCADE)
   private Course course;
 
   @CreationTimestamp
@@ -48,4 +58,26 @@ public class Review {
   @UpdateTimestamp
   @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private Instant updatedAt;
+
+  public Review(Integer rating, String content, User user, Course course) {
+    this.rating = rating;
+    this.content = content;
+    this.user = user;
+    this.course = course;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o))
+      return false;
+    Review review = (Review) o;
+    return id != null && Objects.equals(id, review.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
 }

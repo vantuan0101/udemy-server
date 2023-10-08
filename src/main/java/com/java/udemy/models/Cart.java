f3e80d27509c3e42e61a3c5987c9a5c80e3d1de3
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-10-08 15:20:36 +0700

    update Cart Models

--- src/main/java/com/java/udemy/models/Cart.java
+++ src/main/java/com/java/udemy/models/Cart.java
@@ -5,29 +5,37 @@ import java.time.Instant;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
-@Table(name = "cart")
+@Table(name = "cart", uniqueConstraints = @UniqueConstraint(columnNames = { "user_id", "course_id" }))
 @Getter
 @Setter
+@NoArgsConstructor
 public class Cart {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
-  @JoinColumn(name = "course_id")
+  @ManyToOne(optional = false)
   @JsonBackReference
+  @JoinColumn(name = "course_id")
+  @OnDelete(action = OnDeleteAction.CASCADE)
   private Course course;
 
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id")
+  @OnDelete(action = OnDeleteAction.CASCADE)
   @JsonBackReference
   private User user;
 
@@ -39,4 +47,10 @@ public class Cart {
   @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   @Column(nullable = false)
   private Instant createdAt;
+
+  public Cart(Course course, User user) {
+    this.course = course;
+    this.user = user;
+    this.price = this.course.getPrice();
+  }
 }

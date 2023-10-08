80cab8b7817a51a85348f5085887b7dcb25db23e
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-10-08 15:21:11 +0700

    add EnrollMent models

--- src/main/java/com/java/udemy/models/Enrollment.java
+++ src/main/java/com/java/udemy/models/Enrollment.java
@@ -1,8 +1,11 @@
 package com.java.udemy.models;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Instant;
+import java.util.Objects;
 
+import org.hibernate.Hibernate;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.OnDelete;
@@ -15,23 +18,30 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import javax.validation.constraints.Max;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "enrollments")
 @Getter
 @Setter
+@RequiredArgsConstructor
+@Table(name = "enrollments", uniqueConstraints = @UniqueConstraint(columnNames = { "user_id", "course_id" }))
 public class Enrollment {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
   @JsonBackReference
   @JoinColumn(name = "user_id")
+  @OnDelete(action = OnDeleteAction.CASCADE)
   private User user;
 
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
   @JoinColumn(name = "course_id")
   @JsonBackReference
+  @OnDelete(action = OnDeleteAction.CASCADE)
   private Course course;
 
   @Column(nullable = false)
@@ -54,4 +64,28 @@ public class Enrollment {
   @UpdateTimestamp
   @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private Instant updatedAt = null;
+
+  public Enrollment(User user, Course course) {
+    this.user = user;
+    this.course = course;
+  }
+
+  public BigDecimal getProgress() {
+    return progress.setScale(0, RoundingMode.HALF_UP);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o))
+      return false;
+    Enrollment that = (Enrollment) o;
+    return id != null && Objects.equals(id, that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
 }

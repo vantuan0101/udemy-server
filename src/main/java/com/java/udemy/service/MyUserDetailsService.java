89eac57b68a1f2b2e60df3a83e49698bb11e6bc5
tuanhuynhvan <huynhvantuan889@gmail.com>
2023-10-31 22:36:15 +0700

    add user service

---
+++ src/main/java/com/java/udemy/service/MyUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.java.udemy.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.java.udemy.repository.UserRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+        public static final String USERID = "USER_ID";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found"));
+    }
+}

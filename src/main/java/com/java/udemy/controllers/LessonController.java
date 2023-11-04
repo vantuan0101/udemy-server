5f4a48c7abafc81a0068b5d22a111318bdb5d006
tuanhuynhvan <huynhvantuan889@gmail.com>
2023-11-04 20:18:58 +0700

    add lesson controller

---
+++ src/main/java/com/java/udemy/controllers/LessonController.java
@@ -0,0 +1,41 @@
+package com.java.udemy.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import com.java.udemy.models.Lesson;
+import com.java.udemy.repository.LessonRepository;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/lessons", produces = MediaType.APPLICATION_JSON_VALUE)
+public class LessonController {
+
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @GetMapping(path = "/course/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Slice<Lesson> getLessonsByCourseId(@PathVariable @NotNull Integer id,
+                                              @RequestParam(defaultValue = "0") Integer page) {
+        return lessonRepository.getLessonsByCourseId(id, PageRequest.of(page, 10));
+    }
+
+    @GetMapping(path = "/c/{courseId}/e/{enrollId}")
+    @ResponseStatus(HttpStatus.OK)
+    @Secured(value = "ROLE_STUDENT")
+    public List<Map<String, Object>> getAllMyLessonsInEnrollment(@PathVariable Integer courseId,
+                                                                 @PathVariable Long enrollId) {
+        return lessonRepository.getWatchStatusListByEnrollment(enrollId, courseId);
+    }
+
+}
+

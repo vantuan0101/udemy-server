1746c0875336480ddec28c5420ee81690409fcf0
VanTuan <huynhvantuan889@gmail.com>
2023-11-05 21:12:20 +0700

    add review request

---
+++ src/main/java/com/java/udemy/dto/ReviewRequest.java
@@ -0,0 +1,27 @@
+package com.java.udemy.dto;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.validation.constraints.*;
+
+/**
+ * REVIEW body sent from frontend
+ */
+@Getter
+@ToString
+public class ReviewRequest {
+
+  @NotNull
+  @Min(value = 1, message = "rating cannot be below 1")
+  @Max(value = 5)
+  private Integer rating;
+
+  @Size(max = 300)
+  @NotEmpty
+  private String content;
+
+  @NotNull
+  private Integer courseId;
+
+}

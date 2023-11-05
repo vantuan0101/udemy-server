11c91531e52a5c01025617ad7ca048098105c218
VanTuan <huynhvantuan889@gmail.com>
2023-11-05 21:13:12 +0700

    add sales repo

---
+++ src/main/java/com/java/udemy/repository/SalesRepository.java
@@ -0,0 +1,24 @@
+package com.java.udemy.repository;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.java.udemy.dto.SalesDTO;
+import com.java.udemy.models.Sales;
+
+import java.time.Instant;
+
+@Repository
+public interface SalesRepository extends CrudRepository<Sales, String> {
+
+  Slice<Sales> findByCreatedAtBetween(Instant createdAtStart, Instant createdAtEnd);
+
+  @Query("SELECT new com.java.udemy.dto.SalesDTO(s.transactionId,s.createdAt, s.paymentMethod, s.totalPaid, count(o)) "
+      +
+      "FROM Sales s JOIN OrderItem o ON s.transactionId = o.sale.transactionId WHERE s.user.id = ?1 GROUP BY s.transactionId")
+  Slice<SalesDTO> findByUserIdOrderByCreatedAtDesc(Integer userId, Pageable pageable);
+
+}

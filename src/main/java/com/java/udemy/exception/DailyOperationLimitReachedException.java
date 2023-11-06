fcb1f69ad16aa7533b978e903a542dd4dccc615c
VanTuan <huynhvantuan889@gmail.com>
2023-11-07 00:37:13 +0700

    add DailyOperationLimitReachedException

---
+++ src/main/java/com/java/udemy/exception/DailyOperationLimitReachedException.java
@@ -0,0 +1,11 @@
+package com.java.udemy.exception;
+
+public class DailyOperationLimitReachedException extends RuntimeException {
+
+  private static final long serialVersionUID = -6260854119635270900L;
+
+  public DailyOperationLimitReachedException() {
+    super("Daily transaction limit is reached.");
+  }
+
+}

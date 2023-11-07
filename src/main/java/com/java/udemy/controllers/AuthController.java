f7d852dfec7d9b598616ced6a6877abf4c90950e
VanTuan <huynhvantuan889@gmail.com>
2023-11-08 00:02:29 +0700

    update auth controller

--- src/main/java/com/java/udemy/controllers/AuthController.java
+++ src/main/java/com/java/udemy/controllers/AuthController.java
@@ -2,10 +2,7 @@ package com.java.udemy.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,13 +10,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import com.java.udemy.custom.LoginResponse;
+import com.java.udemy.config.Constants;
 import com.java.udemy.config.security.JwtUtils;
 import com.java.udemy.config.security.UserDetailsImplement;
-import com.java.udemy.custom.GenericResponse;
-import com.java.udemy.dto.LoginRequest;
+import com.java.udemy.exception.BadRequestException;
 import com.java.udemy.models.User;
 import com.java.udemy.repository.UserRepository;
+import com.java.udemy.request.LoginRequest;
+import com.java.udemy.response.GenericResponse;
+import com.java.udemy.response.LoginResponse;
 import com.java.udemy.service.concretions.UserService;
 
 import jakarta.servlet.http.HttpSession;
@@ -53,45 +52,44 @@ public class AuthController {
     }
 
     @PostMapping(path = "/register")
-    public ResponseEntity<GenericResponse> addNewUser(@RequestBody @Valid User user) {
-        if (!user.getPassword().equals(user.getConfirmPass()))
-            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
-                    .body(GenericResponse.fail("Passwords don't match"));
-
+    public GenericResponse addNewUser(@RequestBody @Valid User user) {
         try {
+            if (!user.getPassword().equals(user.getConfirmPass()))
+                throw new BadRequestException(Constants.MESSAGE_INVALID_MATCH_PASSWORD);
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             userRepository.save(user);
-            return ResponseEntity.status(HttpStatus.CREATED).body(new GenericResponse("Registered! Welcome"));
+            GenericResponse response = new GenericResponse(Constants.MESSAGE_REGISTER_WELCOME);
+            return response;
         } catch (Exception ex) {
-            if (ex instanceof DataIntegrityViolationException) {
-                return ResponseEntity.status(HttpStatus.CONFLICT).body(GenericResponse.fail("Account already exists"));
-            }
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(GenericResponse.fail(ex.getMessage()));
+            throw new BadRequestException(ex.getMessage());
         }
     }
 
     @PostMapping(path = "/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest, HttpSession httpSession) {
-        String password = loginRequest.getPassword();
-        String email = loginRequest.getEmail();
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(email, password));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
-        UserDetailsImplement userDetails = (UserDetailsImplement) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
-                .collect(Collectors.toList());
-
-        httpSession.setAttribute(UserService.USERID, userDetails.getId());
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new LoginResponse(
-                        "Login Successfully!",
-                        jwt,
-                        userDetails.getId(),
-                        userDetails.getUsername(),
-                        userDetails.getEmail(),
-                        roles));
+    public LoginResponse login(@RequestBody @Valid LoginRequest loginRequest, HttpSession httpSession) {
+        try {
+            String password = loginRequest.getPassword();
+            String email = loginRequest.getEmail();
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(email, password));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String jwt = jwtUtils.generateJwtToken(authentication);
+            UserDetailsImplement userDetails = (UserDetailsImplement) authentication.getPrincipal();
+            List<String> roles = userDetails.getAuthorities().stream()
+                    .map(item -> item.getAuthority())
+                    .collect(Collectors.toList());
+            httpSession.setAttribute(UserService.USERID, userDetails.getId());
+            LoginResponse response = new LoginResponse(
+                    Constants.MESSAGE_LOGIN_SUCCESS,
+                    jwt,
+                    userDetails.getId(),
+                    userDetails.getUsername(),
+                    userDetails.getEmail(),
+                    roles);
+            return response;
+        } catch (Exception ex) {
+            throw new BadRequestException(ex.getMessage());
+        }
 
     }
 

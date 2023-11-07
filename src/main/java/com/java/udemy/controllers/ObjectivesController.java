35567218af5f3a1c1952647a043bba1413decfc0
VanTuan <huynhvantuan889@gmail.com>
2023-11-08 00:03:29 +0700

    update objective controller

--- src/main/java/com/java/udemy/controllers/ObjectivesController.java
+++ src/main/java/com/java/udemy/controllers/ObjectivesController.java
@@ -3,17 +3,18 @@ package com.java.udemy.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.java.udemy.custom.GenericResponse;
-import com.java.udemy.dto.ObjectivesDTO;
+import com.java.udemy.exception.BadRequestException;
 import com.java.udemy.models.Course;
 import com.java.udemy.models.CourseObjective;
 import com.java.udemy.repository.CourseRepository;
 import com.java.udemy.repository.ObjectiveRepository;
+import com.java.udemy.request.CreateObjectivesRequest;
+import com.java.udemy.response.CreateObjectivesResponse;
+import com.java.udemy.response.GetCourseObjectivesResponse;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -31,21 +32,30 @@ public class ObjectivesController {
 
   @PostMapping(value = "/")
   @Secured(value = "ROLE_ADMIN")
-  public ResponseEntity<GenericResponse> addNewObjectives(@RequestBody @Valid ObjectivesDTO objDTO) {
-    List<String> objectives = objDTO.getObjectives();
+  public CreateObjectivesResponse addNewObjectives(@RequestBody @Valid CreateObjectivesRequest request) {
+    List<String> objectives = request.getObjectives();
     try {
-      Course course = courseRepository.findById(objDTO.getCourseId()).orElseThrow();
+      Course course = courseRepository.findById(request.getCourseId()).orElseThrow();
       List<CourseObjective> coList = objectives.stream().map(o -> new CourseObjective(course, o))
           .collect(Collectors.toList());
       objectiveRepository.saveAll(coList);
-      return ResponseEntity.status(HttpStatus.CREATED).body(new GenericResponse("All saved!"));
+      CreateObjectivesResponse response = new CreateObjectivesResponse();
+      response.setMessage("All saved!");
+      return response;
     } catch (Exception e) {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not save new objectives", e);
     }
   }
 
   @GetMapping(value = "/course/{courseId}")
-  public List<CourseObjective> getCourseObjectives(@PathVariable Integer courseId) {
-    return objectiveRepository.getCourseObjectivesByCourseId(courseId);
+  public GetCourseObjectivesResponse getCourseObjectives(@PathVariable Integer courseId) {
+    try {
+      List<CourseObjective> courseObjectivesList = objectiveRepository.getCourseObjectivesByCourseId(courseId);
+      GetCourseObjectivesResponse response = new GetCourseObjectivesResponse();
+      response.setGetCourseObjectives(courseObjectivesList);
+      return response;
+    } catch (Exception ex) {
+      throw new BadRequestException(ex.getMessage());
+    }
   }
 }

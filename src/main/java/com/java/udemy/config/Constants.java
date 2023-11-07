51b98a4ab7e3e2a62a6b419a5d88fa086d55c87a
VanTuan <huynhvantuan889@gmail.com>
2023-11-08 00:01:56 +0700

    add constant

--- src/main/java/com/java/udemy/config/Constants.java
+++ src/main/java/com/java/udemy/config/Constants.java
@@ -7,11 +7,14 @@ public final class Constants {
 
   public static final String MAIN_CURRENCY = "TRY";
 
-  public static final String MESSAGE_INVALIDUSERNAME = "Invalid username.";
-  public static final String MESSAGE_INVALIDPASSWORD = "Invalid password.";
-  public static final String MESSAGE_INVALIDCURRENCY = "Invalid currency.";
-  public static final String MESSAGE_INVALIDAMOUNT = "Invalid amount.";
-  public static final String MESSAGE_INVALIDTCNO = "Invalid TC No.";
+  public static final String MESSAGE_REGISTER_WELCOME = "Registered! Welcome.";
+  public static final String MESSAGE_LOGIN_SUCCESS = "Login Successfully!.";
+  public static final String MESSAGE_INVALID_USERNAME = "Invalid username.";
+  public static final String MESSAGE_INVALID_PASSWORD = "Invalid password.";
+  public static final String MESSAGE_INVALID_MATCH_PASSWORD = "Passwords don't match.";
+  public static final String MESSAGE_INVALID_CURRENCY = "Invalid currency.";
+  public static final String MESSAGE_INVALID_AMOUNT = "Invalid amount.";
+  public static final String MESSAGE_INVALID_TCNO = "Invalid TC No.";
   public static final String MESSAGE_EXCEEDEDMAXVALUE = "Exceeded Maximum Value Per Transaction.";
   public static final String MESSAGE_EXCEEDEDMAXVALUEFORDAY = "Exceeded Maximum Transaction Value For the Day.";
   public static final String MESSAGE_SAMEUSERTRANSACTION = "You can't send money to yourself.";

fa089891e4532a4cf4d8109b75ef5817faf40a9a
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-11-08 09:55:08 +0700

    update UserRequest

--- src/main/java/com/java/udemy/request/UserRequest.java
+++ src/main/java/com/java/udemy/request/UserRequest.java
@@ -7,7 +7,7 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-public class UserRequest {
+public class UserRequest extends BaseRequest {
   private Integer id;
   private String fullname;
   private String email;

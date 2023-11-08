4aeb0336dc33d691f938efdaced44c3f2252b83d
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-11-08 09:54:41 +0700

    update OrderItemRequest

--- src/main/java/com/java/udemy/request/OrderItemRequest.java
+++ src/main/java/com/java/udemy/request/OrderItemRequest.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @Setter
 @RequiredArgsConstructor
 @ToString
-public class OrderItemRequest {
+public class OrderItemRequest extends BaseRequest {
   private Long id;
   private String title;
   private BigDecimal price;

d0b3acb4cbf222ab0ee821784c55aefc7bca995b
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-11-09 11:18:55 +0700

    update swagger doc

--- src/main/java/com/java/udemy/config/SecurityConfig.java
+++ src/main/java/com/java/udemy/config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
-
 import com.java.udemy.config.security.AuthTokenFilter;
 
 @Configuration

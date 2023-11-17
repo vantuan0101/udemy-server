786070b199c842595e4169f91986a9b2e68f8b15
VanTuan <huynhvantuan889@gmail.com>
2023-11-17 10:57:09 +0700

    update IUserService

--- src/main/java/com/java/udemy/service/abstractions/IUserService.java
+++ src/main/java/com/java/udemy/service/abstractions/IUserService.java
@@ -1,5 +1,11 @@
 package com.java.udemy.service.abstractions;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.servlet.http.HttpSession;
+
 public interface IUserService {
-  Integer getSessionUserId();
+  Integer getSessionUserId(HttpSession session);
+
+  UserDetails loadUserByUsername(String username);
 }

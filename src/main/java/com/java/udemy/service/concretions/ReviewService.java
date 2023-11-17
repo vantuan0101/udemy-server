38641fffac90e3d91d59e69cafcdc63f80942ac9
VanTuan <huynhvantuan889@gmail.com>
2023-11-17 10:59:04 +0700

    update ReviewService

--- src/main/java/com/java/udemy/service/concretions/ReviewService.java
+++ src/main/java/com/java/udemy/service/concretions/ReviewService.java
@@ -1,8 +1,14 @@
 package com.java.udemy.service.concretions;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.java.udemy.models.Course;
 import com.java.udemy.models.Review;
@@ -11,12 +17,15 @@ import com.java.udemy.repository.CourseRepository;
 import com.java.udemy.repository.ReviewRepository;
 import com.java.udemy.repository.UserRepository;
 import com.java.udemy.request.ReviewRequest;
+import com.java.udemy.service.abstractions.IReviewService;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
-public class ReviewService {
+public class ReviewService implements IReviewService {
 
   @Autowired
   private CourseRepository courseRepository;
@@ -35,6 +44,7 @@ public class ReviewService {
    * @param request from frontend
    * @param userId  userId
    */
+  @Override
   @Transactional
   public void addCourseRating(ReviewRequest request, Integer userId) {
     User user = userRepository.findById(userId).orElseThrow();
@@ -51,6 +61,7 @@ public class ReviewService {
   /**
    * EDIT existing review, and modify AVG course rating
    */
+  @Override
   @Transactional
   public void updateCourseRating(Integer reviewId, ReviewRequest request) {
     Review myReview = reviewRepository.findById(reviewId).orElseThrow();
@@ -63,5 +74,21 @@ public class ReviewService {
     double avgRating = reviewRepository.getAverageByCourseId(course.getId());
     course.setRating(BigDecimal.valueOf(avgRating).setScale(2, RoundingMode.DOWN));
     courseRepository.save(course);
+  }
+
+  @Override
+  public Optional<Review> findByUserIdAndCourseId(Integer userId, Integer courseId) {
+    Optional<Review> reviewOptional = reviewRepository.findByUserIdAndCourseId(userId, courseId);
+    return reviewOptional;
+  }
+
+  @Override
+  public Slice<ReviewRequest> findByCourseId(Integer page, String sortBy, Integer courseId) {
+    if (Stream.of("createdAt", "rating").noneMatch(sortBy::equals)) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid 'sort' param");
+    }
+    Pageable pageable = PageRequest.of(page, 10, Sort.Direction.DESC, sortBy);
+    Slice<ReviewRequest> reviews = reviewRepository.findByCourseId(courseId, pageable);
+    return reviews;
   }
 }

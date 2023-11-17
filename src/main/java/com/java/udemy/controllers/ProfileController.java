8afa587e03509da5b8463497e6a80a74ad8decdd
VanTuan <huynhvantuan889@gmail.com>
2023-11-17 21:32:41 +0700

    update profile controller

--- src/main/java/com/java/udemy/controllers/ProfileController.java
+++ src/main/java/com/java/udemy/controllers/ProfileController.java
@@ -2,7 +2,6 @@ package com.java.udemy.controllers;
 
 import javax.validation.constraints.NotNull;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.java.udemy.models.User;
-import com.java.udemy.repository.UserRepository;
 import com.java.udemy.request.UserRequest;
 import com.java.udemy.response.EditMyProfileResponse;
 import com.java.udemy.response.GetUserByIdResponse;
@@ -25,22 +22,15 @@ import jakarta.servlet.http.HttpSession;
 @RestController
 @RequestMapping(path = "/profile")
 public class ProfileController {
-  private final UserRepository userRepository;
-  private final ModelMapper modelMapper;
-  private IUserService userService;
 
   @Autowired
-  public ProfileController(UserRepository userRepository, IUserService userService) {
-    this.userRepository = userRepository;
-    this.userService = userService;
-    this.modelMapper = new ModelMapper();
-  }
+  private IUserService userService;
 
   @GetMapping(path = "/me")
   public GetUserByIdResponse getUserById(@NotNull HttpSession session) {
     try {
       Integer userId = userService.getSessionUserId(session);
-      UserRequest userDTO = userRepository.findUserDTObyId(userId).orElseThrow();
+      UserRequest userDTO = userService.findUserById(userId);
       GetUserByIdResponse response = new GetUserByIdResponse();
       response.setUserDTO(userDTO);
       return response;
@@ -54,11 +44,7 @@ public class ProfileController {
   public EditMyProfileResponse editMyProfile(@RequestBody UserRequest request, @NotNull HttpSession session) {
     try {
       Integer userId = userService.getSessionUserId(session);
-      User user = userRepository.findById(userId).orElseThrow();
-      user.setFullname(request.getFullname());
-      user.setConfirmPass("WHATEVER!");
-      User freshUser = userRepository.save(user);
-      UserRequest userDTO = modelMapper.map(freshUser, UserRequest.class);
+      UserRequest userDTO = userService.updateProfileById(request, userId);
       EditMyProfileResponse response = new EditMyProfileResponse();
       response.setUserDTO(userDTO);
       return response;

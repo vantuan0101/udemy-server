5c4ab267b59fcb0135305b69c2d43774c742e3e2
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-12-19 15:46:36 +0700

    remove getWatchStatusListByEnrollment

--- src/main/java/com/java/udemy/service/abstractions/ILessonService.java
+++ src/main/java/com/java/udemy/service/abstractions/ILessonService.java
@@ -6,9 +6,13 @@ import java.util.Map;
 import org.springframework.data.domain.Slice;
 
 import com.java.udemy.models.Lesson;
+import com.java.udemy.request.LessonRequest;
 
 public interface ILessonService {
   Slice<Lesson> getLessonsByCourseId(Integer id, Integer page);
 
-  List<Map<String, Object>> getWatchStatusListByEnrollment(Integer courseId, Long enrollId);
+  // List<Map<String, Object>> getWatchStatusListByEnrollment(Integer courseId,
+  // Long enrollId);
+
+  Lesson createLesson(LessonRequest request);
 }

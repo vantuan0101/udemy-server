67b36d8921be9f0d2e43a0baccd499119e269c96
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-12-19 15:46:55 +0700

    update lesson service

--- src/main/java/com/java/udemy/service/concretions/LessonService.java
+++ src/main/java/com/java/udemy/service/concretions/LessonService.java
@@ -8,14 +8,19 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
+import com.java.udemy.models.Course;
 import com.java.udemy.models.Lesson;
+import com.java.udemy.repository.CourseRepository;
 import com.java.udemy.repository.LessonRepository;
+import com.java.udemy.request.LessonRequest;
 import com.java.udemy.service.abstractions.ILessonService;
 
 @Service
 public class LessonService implements ILessonService {
   @Autowired
   private LessonRepository lessonRepository;
+  @Autowired
+  private CourseRepository courseRepository;
 
   @Override
   public Slice<Lesson> getLessonsByCourseId(Integer id, Integer page) {
@@ -23,9 +28,31 @@ public class LessonService implements ILessonService {
     return lessons;
   }
 
+  // @Override
+  // public List<Map<String, Object>> getWatchStatusListByEnrollment(Integer
+  // courseId, Long enrollId) {
+  // List<Map<String, Object>> lessons =
+  // lessonRepository.getWatchStatusListByEnrollment(enrollId, courseId);
+  // return lessons;
+  // }
+
   @Override
-  public List<Map<String, Object>> getWatchStatusListByEnrollment(Integer courseId, Long enrollId) {
-    List<Map<String, Object>> lessons = lessonRepository.getWatchStatusListByEnrollment(enrollId, courseId);
+  public Lesson createLesson(LessonRequest request) {
+    Course course = courseRepository.findById(request.getCourseId()).orElseThrow();
+    System.out.println("Course: " + course);
+    // Lesson lesson = new Lesson(
+    // request.getLessonName(),
+    // request.getVideo_url(),
+    // request.getVideokey(),
+    // request.getLengthSeconds(),
+    // request.getPosition(),
+    // course);
+    Lesson lesson = new Lesson(
+        request.getLessonName(),
+        request.getVideo_url(),
+        request.getPosition(),
+        course);
+    Lesson lessons = lessonRepository.save(lesson);
     return lessons;
   }
 }

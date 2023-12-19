5a288b98c3cd99f858beb0f39f6bff02e93639af
tuanhuynhvan-itr <tuanhuynhvan@itrvn.com>
2023-12-19 15:47:07 +0700

    update createLesson

--- src/main/java/com/java/udemy/controllers/LessonController.java
+++ src/main/java/com/java/udemy/controllers/LessonController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import com.java.udemy.exception.BadRequestException;
 import com.java.udemy.models.Lesson;
+import com.java.udemy.request.LessonRequest;
 import com.java.udemy.response.GetAllMyLessonsInEnrollmentResponse;
 import com.java.udemy.response.GetLessonsByCourseIdResponse;
 import com.java.udemy.service.abstractions.ILessonService;
@@ -39,16 +41,29 @@ public class LessonController {
 
     }
 
-    @GetMapping(path = "/c/{courseId}/e/{enrollId}")
-    @ResponseStatus(HttpStatus.OK)
-    @Secured(value = "ROLE_STUDENT")
-    public GetAllMyLessonsInEnrollmentResponse getAllMyLessonsInEnrollment(@PathVariable Integer courseId,
-            @PathVariable Long enrollId) {
+    // @GetMapping(path = "/c/{courseId}/e/{enrollId}")
+    // @ResponseStatus(HttpStatus.OK)
+    // @Secured(value = "ROLE_STUDENT")
+    // public GetAllMyLessonsInEnrollmentResponse
+    // getAllMyLessonsInEnrollment(@PathVariable Integer courseId,
+    // @PathVariable Long enrollId) {
+    // try {
+    // List<Map<String, Object>> lessons =
+    // lessonService.getWatchStatusListByEnrollment(courseId, enrollId);
+    // GetAllMyLessonsInEnrollmentResponse response = new
+    // GetAllMyLessonsInEnrollmentResponse();
+    // response.setGetAllMyLessonsInEnrollment(lessons);
+    // return response;
+    // } catch (Exception ex) {
+    // throw new BadRequestException(ex.getMessage());
+    // }
+    // }
+
+    @PostMapping()
+    public ResponseEntity<Lesson> createLesson(@RequestBody LessonRequest request) {
         try {
-            List<Map<String, Object>> lessons = lessonService.getWatchStatusListByEnrollment(courseId, enrollId);
-            GetAllMyLessonsInEnrollmentResponse response = new GetAllMyLessonsInEnrollmentResponse();
-            response.setGetAllMyLessonsInEnrollment(lessons);
-            return response;
+            Lesson lesson = lessonService.createLesson(request);
+            return ResponseEntity.ok().body(lesson);
         } catch (Exception ex) {
             throw new BadRequestException(ex.getMessage());
         }

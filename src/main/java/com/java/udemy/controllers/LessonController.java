10ac2001e04ffbeaf81c5d89238efc11a07e8ae7
VanTuan <huynhvantuan889@gmail.com>
2023-12-27 23:26:49 +0700

    update LessonController

--- src/main/java/com/java/udemy/controllers/LessonController.java
+++ src/main/java/com/java/udemy/controllers/LessonController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.*;
 import com.java.udemy.exception.BadRequestException;
 import com.java.udemy.models.Lesson;
 import com.java.udemy.request.LessonRequest;
-import com.java.udemy.response.GetAllMyLessonsInEnrollmentResponse;
 import com.java.udemy.response.GetLessonsByCourseIdResponse;
 import com.java.udemy.service.abstractions.ILessonService;
+import com.java.udemy.service.abstractions.IUserService;
+
+import jakarta.servlet.http.HttpSession;
 
 import javax.validation.constraints.NotNull;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/lessons", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -25,6 +25,8 @@ public class LessonController {
 
     @Autowired
     private ILessonService lessonService;
+    @Autowired
+    private IUserService userService;
 
     @GetMapping(path = "/course/{id}")
     @ResponseStatus(HttpStatus.OK)
@@ -60,13 +62,41 @@ public class LessonController {
     // }
 
     @PostMapping()
-    public ResponseEntity<Lesson> createLesson(@RequestBody LessonRequest request) {
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<Lesson> createLesson(@RequestBody LessonRequest request, @NotNull HttpSession session) {
         try {
-            Lesson lesson = lessonService.createLesson(request);
+            Integer userId = userService.getSessionUserId(session);
+            Lesson lesson = lessonService.createLesson(request, userId);
             return ResponseEntity.ok().body(lesson);
         } catch (Exception ex) {
             throw new BadRequestException(ex.getMessage());
         }
     }
 
+    @Secured({ "ROLE_TEACHER", "ROLE_ADMIN" })
+    @PutMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public ResponseEntity<Lesson> updateLesson(@RequestBody LessonRequest request, @PathVariable Integer id,
+            @NotNull HttpSession session) {
+        try {
+            Integer userId = userService.getSessionUserId(session);
+            Lesson lesson = lessonService.updateLesson(request, userId, id);
+            return ResponseEntity.ok().body(lesson);
+        } catch (Exception ex) {
+            throw new BadRequestException(ex.getMessage());
+        }
+    }
+
+    @Secured({ "ROLE_TEACHER", "ROLE_ADMIN" })
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void deleteLesson(@PathVariable Integer id, @NotNull HttpSession session) {
+        try {
+            Integer userId = userService.getSessionUserId(session);
+            lessonService.deleteLesson(id, userId);
+        } catch (Exception ex) {
+            throw new BadRequestException(ex.getMessage());
+        }
+
+    }
 }

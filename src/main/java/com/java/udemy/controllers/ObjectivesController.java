d7db177660e508bb8bfc1ba129b838e4e173e91a
VanTuan <huynhvantuan889@gmail.com>
2023-12-27 23:27:00 +0700

    update ObjectivesController

--- src/main/java/com/java/udemy/controllers/ObjectivesController.java
+++ src/main/java/com/java/udemy/controllers/ObjectivesController.java
@@ -14,8 +14,13 @@ import com.java.udemy.request.CreateObjectivesRequest;
 import com.java.udemy.response.CreateObjectivesResponse;
 import com.java.udemy.response.GetCourseObjectivesResponse;
 import com.java.udemy.service.abstractions.IObjectivesService;
+import com.java.udemy.service.abstractions.IUserService;
+
+import jakarta.servlet.http.HttpSession;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 import java.util.List;
 
 @RestController
@@ -25,17 +30,35 @@ public class ObjectivesController {
   @Autowired
   private IObjectivesService objectivesService;
 
+  @Autowired
+  private IUserService userService;
+
   @PostMapping(value = "/")
   @Secured(value = "ROLE_ADMIN")
-  public CreateObjectivesResponse addNewObjectives(@RequestBody @Valid CreateObjectivesRequest request) {
+  public CreateObjectivesResponse addNewObjectives(@RequestBody @Valid CreateObjectivesRequest request,
+      @NotNull HttpSession session) {
     try {
-      List<CourseObjective> coList = objectivesService.createObjectives(request);
+      Integer userId = userService.getSessionUserId(session);
+      List<CourseObjective> coList = objectivesService.createObjectives(request, userId);
       CreateObjectivesResponse response = new CreateObjectivesResponse();
       response.setMessage(Constants.MESSAGE_ALL_SAVE);
       return response;
     } catch (Exception e) {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, Constants.MESSAGE_CAN_NOT_SAVE_OBJECTIVE, e);
     }
+  }
+
+  @Secured({ "ROLE_TEACHER", "ROLE_ADMIN" })
+  @DeleteMapping("/{id}")
+  @ResponseStatus(value = HttpStatus.OK)
+  public void deleteObjectives(@PathVariable Integer id, @NotNull HttpSession session) {
+    try {
+      Integer userId = userService.getSessionUserId(session);
+      objectivesService.deleteObjectives(id, userId);
+    } catch (Exception ex) {
+      throw new BadRequestException(ex.getMessage());
+    }
+
   }
 
   @GetMapping(value = "/course/{courseId}")
